33e216d1e53b644f11b97793158c890c44bc6aec
Duong Dang <dddangmedia@gmail.com>
2019-09-11 19:12:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-
+dwarves = ["Dopey", "Grumpy", "Bashful"]
+roll_call_dwarves(dwarves)
 
 def summon_captain_planet# code an argument here
   # Your code here

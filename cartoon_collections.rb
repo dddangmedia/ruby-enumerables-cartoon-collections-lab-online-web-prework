280f7431b9d45daf938d7c6e06573fd788d3c69c
Duong Dang <dddangmedia@gmail.com>
2019-09-11 19:10:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(dwarves)
-  dwarves.sort.each_with_index {|name, index| p "#{index + 1} #{name}"}
+  dwarves.sort.each_with_index do |name, index| 
+    puts "#{index + 1} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here

69fe7fec74083deb91867b08fc4eeb4659058b6e
Duong Dang <dddangmedia@gmail.com>
2019-09-11 19:42:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  p snacks.include(cheese_types)
 end
